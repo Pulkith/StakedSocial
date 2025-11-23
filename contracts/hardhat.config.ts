d5f1305a2eb1f274e7b1b5aa275fd22680641363
Celo Composer <support@celo.org>
2025-11-23 04:53:11 -0300

    Deployed COntract

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -21,7 +21,7 @@ const config: HardhatUserConfig = {
         mnemonic: process.env.MNEMONIC!,
         initialIndex: 0,
         count: 1,
-        path: "m/44'/52752'/0'/0",
+        path: "m/44'/60'/0'/0",
       },
     },
 
